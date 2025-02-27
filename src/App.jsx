29376a70e9e5580d2d30a9ecb9b28dbbb56b2519
L0rD-Op <office.snj.2005@gmail.com>
2025-02-27 18:37:47 +0530

    Components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-
+import Navbar from "./components/navbar"
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+      {/*<Navbar/>*/}
+      {/* Remove the code below and keep the app clean and use components folder to add changes, keep the medias in the assets*/}
       <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
         <h1 className="text-4xl font-bold text-white mb-4">Welcome to React!</h1>
         <p className="text-gray-300 text-lg">Start building your project here</p>
+
       </div>
     </div>
   )
