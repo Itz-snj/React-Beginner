c7f086d3c2bdcd09e8cb9d6f25921c78b10f69de
Ashmita001 <ashmita.202004@gmail.com>
2025-03-16 19:23:56 +0530

    working-process-added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/home";
 import Services from "./components/services";
+import WorkingProcess from "./components/WorkingProcess";
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Services/>
+      <WorkingProcess/>
     </div>
   );
 }
