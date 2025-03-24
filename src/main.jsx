8424bf29b741b04ee10b6bf0be8f550d85b8b39a
Saptarshi Paul <saptarshi2027paul@gmail.com>
2025-03-25 00:06:00 +0530

    solved #22

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Landing from './components/landing'
+import { BrowserRouter , Route , Routes } from 'react-router'
+import Login from './components/Login'
+import Register from './components/Register'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  <Routes>
+    <Route path='/'  element={<Landing/>} ></Route>
+    <Route path='/login' element={<Login/>} ></Route>
+    <Route path='/register' element={<Register/>} ></Route>
+  </Routes>
+  </BrowserRouter>
 )
