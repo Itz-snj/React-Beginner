8f1b6e738e4f8bfe8edf58c983ac32ab12165554
Ashmita001 <ashmita.202004@gmail.com>
2025-03-26 04:25:50 +0530

    resolved error

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+
+  build: {
+    chunkSizeWarningLimit: 600, // Adjust limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Separate dependencies into "vendor.js"
+          }
+        },
+      },
+    },
+  },
 })
