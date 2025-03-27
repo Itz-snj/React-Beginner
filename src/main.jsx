6c48f5df4f60aee43a77aea6f4159cda9c3804a6
Suman Jain <office.snj.2005@gmail.com>
2025-03-27 13:05:11 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Landing from './components/landing'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './components/Login'
+import Login from './components/login'
 import Register from './components/Register'
 import { AuthProvider } from './context/AuthContext'
 
